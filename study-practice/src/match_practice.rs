8ab199f9e7d91597e30ee8c94d3f401126750570
Trackerming <1591724804@qq.com>
2023-09-27 11:08:50 +0800

    [rust的练习模块]fix添加match相关的练习

--- study-practice/src/match_practice.rs
+++ study-practice/src/match_practice.rs
@@ -11,10 +11,11 @@ fn method_1() {
     let dire = Direction::South;
     match dire {
         Direction::East => println!("East"),
-        __  => { // 在这里匹配 South 或 North
+        Direction::South | Direction::North => {
+            // 在这里匹配 South 或 North
             println!("South or North");
-        },
-        _ => println!(__),
+        }
+        _ => println!("West"),
     };
 }
 
@@ -25,11 +26,13 @@ fn method_2() {
     //
     // boolean = true => binary = 1
     // boolean = false => binary = 0
-    let binary = __;
+    let binary = match boolean {
+        true => 1,
+        false => 0,
+    };
 
     assert_eq!(binary, 1);
 }
-
 
 // 填空
 enum Message {
@@ -42,50 +45,51 @@ enum Message {
 fn method_3() {
     let msgs = [
         Message::Quit,
-        Message::Move{x:1, y:3},
-        Message::ChangeColor(255,255,0)
+        Message::Move { x: 1, y: 3 },
+        Message::ChangeColor(255, 255, 0),
     ];
 
     for msg in msgs {
         show_message(msg)
     }
-} 
+}
 
 fn show_message(msg: Message) {
     match msg {
-        __ => { // 这里匹配 Message::Move
+        Message::Move { x: a, y: b } => {
+            // 这里匹配 Message::Move
             assert_eq!(a, 1);
             assert_eq!(b, 3);
-        },
-        Message::ChangeColor(_, g, b) => {
-            assert_eq!(g, __);
-            assert_eq!(b, __);
         }
-        __ => println!("no data in these variants")
+        Message::ChangeColor(_, g, b) => {
+            assert_eq!(g, 255);
+            assert_eq!(b, 0);
+        }
+        __ => println!("no data in these variants"),
     }
 }
 
-
 fn method_4() {
-    let alphabets = ['a', 'E', 'Z', '0', 'x', '9' , 'Y'];
+    let alphabets = ['a', 'E', 'Z', '0', 'x', '9', 'Y'];
 
     // 使用 `matches` 填空
     for ab in alphabets {
-        assert!(__)
+        assert!(matches!(ab, char))
     }
-} 
+}
 
 enum MyEnum {
     Foo,
-    Bar
+    Bar,
 }
 
 fn method_5() {
     let mut count = 0;
 
-    let v = vec![MyEnum::Foo,MyEnum::Bar,MyEnum::Foo];
+    let v = vec![MyEnum::Foo, MyEnum::Bar, MyEnum::Foo];
     for e in v {
-        if e == MyEnum::Foo { // 修复错误，只能修改本行代码
+        if matches!(e, MyEnum::Foo) {
+            // 修复错误，只能修改本行代码
             count += 1;
         }
     }
@@ -97,23 +101,20 @@ fn method_6() {
     let o = Some(7);
 
     // 移除整个 `match` 语句块，使用 `if let` 替代
-    match o {
-        Some(i) => {
-            println!("This is a really long string and `{:?}`", i);
-        }
-        _ => {}
-    };
+    if let Some(i) = o {
+        println!("This is a really long string and `{:?}`", i);
+    }
 }
 
 // 填空
 enum Foo {
-    Bar(u8)
+    Bar(u8),
 }
 
 fn method_7() {
     let a = Foo::Bar(1);
 
-    __ {
+    if let Foo::Bar(i) = a {
         println!("foobar 持有的值是: {}", i);
     }
 }
@@ -121,33 +122,35 @@ fn method_7() {
 enum Foo8 {
     Bar,
     Baz,
-    Qux(u32)
+    Qux(u32),
 }
 
 fn method_8() {
     let a = Foo8::Qux(10);
 
     // 移除以下代码，使用 `match` 代替
-    if let Foo8::Bar = a {
-        println!("match foo::bar")
-    } else if let Foo8::Baz = a {
-        println!("match foo::baz")
-    } else {
-        println!("match others")
+    match a {
+        Foo8::Bar => println!("match foo::bar"),
+        Foo8::Baz => println!("match foo::baz"),
+        others => println!("match others"),
     }
 }
 
 // 就地修复错误
 fn method_9() {
     let age = Some(30);
-    if let Some(age) = age { // 创建一个新的变量，该变量与之前的 `age` 变量同名
-        assert_eq!(age, Some(30));
+    if let Some(age) = age {
+        // 创建一个新的变量，该变量与之前的 `age` 变量同名
+        assert_eq!(age, 30);
     } // 新的 `age` 变量在这里超出作用域
 
     match age {
         // `match` 也能实现变量遮蔽
-        Some(age) =>  println!("age 是一个新的变量，它的值是 {}",age),
-        _ => ()
+        Some(age) => {
+            println!("age 是一个新的变量，它的值是 {}", age);
+            assert_eq!(age, 30);
+        }
+        _ => (),
     }
 }
 
