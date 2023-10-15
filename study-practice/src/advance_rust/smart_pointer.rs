b73c5529229e2f35e7360e2cdae4953523a9acb9
Trackerming <1591724804@qq.com>
2023-10-15 14:26:01 +0800

    [rust的练习模块]添加Drop特征及智能指针如Rc、Arc、Cell、RefCell的相关示例

--- study-practice/src/advance_rust/smart_pointer.rs
+++ study-practice/src/advance_rust/smart_pointer.rs
@@ -133,9 +133,143 @@ fn deref_example() {
     (&&&&&f).foo(5);
 }
 
+/// Drop特征
+
+/// RC和Arc
+fn rc_example() {
+    let a = Rc::new(String::from("rust"));
+    // 这里的 clone 仅仅复制了智能指针并增加了引用计数，并没有克隆底层数据
+    let b = Rc::clone(&a);
+    assert_eq!(2, Rc::strong_count(&a));
+    assert_eq!(Rc::strong_count(&a), Rc::strong_count(&b));
+}
+struct Owner {
+    name: String,
+}
+struct Gadget {
+    id: i32,
+    owner: Rc<Owner>,
+}
+fn example_rc_all() {
+    let gadget_owner: Rc<Owner> = Rc::new(Owner {
+        name: "Rust".to_string(),
+    });
+    let gadget1 = Gadget {
+        id: 1,
+        owner: gadget_owner.clone(),
+    };
+    let gadget2 = Gadget {
+        id: 2,
+        owner: Rc::clone(&gadget_owner),
+    };
+    let gadget3 = Gadget {
+        id: 3,
+        owner: gadget_owner.clone(),
+    };
+    drop(gadget_owner);
+    println!("Gadget count {}", Rc::strong_count(&gadget1.owner));
+    println!("Gadget {} owned by {}", gadget1.id, gadget1.owner.name);
+    println!("Gadget {} owned by {}", gadget2.id, gadget2.owner.name);
+    println!("Gadget {} owned by {}", gadget3.id, gadget3.owner.name);
+}
+use std::sync::Arc;
+use std::thread;
+// Arc 线程安全的引用计数，与Rc拥有一样的API
+fn arc_example() {
+    let a_str = Arc::new(String::from("Arc example"));
+    for i in 0..10 {
+        let a_str_clone = Arc::clone(&a_str);
+        let current_i = i;
+        thread::spawn(move || {
+            // let a_str_clone = a_str.clone();
+            println!("another {current_i} thread {}", a_str_clone);
+        });
+    }
+}
+
+/// Cell和RefCell
+use std::cell::Cell;
+fn example_cell() {
+    let c = Cell::new("rust cell");
+    let one = c.get();
+    c.set("rust cell edit.");
+    let two = c.get();
+    println!("one {}, two {}", one, two);
+    let x = Cell::new(24);
+    let y = &x;
+    let z = &x;
+    x.set(24 + 1);
+    y.set(24 + 2);
+    z.set(24 + 3);
+    println!("x: {}, y: {}. z: {}", x.get(), y.get(), z.get());
+}
+use std::cell::RefCell;
+// 将不可变引用转化为可变引用
+trait Message {
+    fn send(&self, msg: String);
+}
+#[derive(Debug)]
+struct MessageQueue {
+    msg_cache: RefCell<Vec<String>>,
+}
+impl Message for MessageQueue {
+    fn send(&self, msg: String) {
+        self.msg_cache.borrow_mut().push(msg);
+    }
+}
+fn refcell_example() {
+    let message_queue = MessageQueue {
+        msg_cache: RefCell::new(vec!["rust".to_string(), "refcell".to_string()]),
+    };
+    message_queue.send("hello".to_string());
+    println!("message queue: {:?}", message_queue);
+}
+
+fn refcell_rc_mix() {
+    let str0 = Rc::new(RefCell::new("multi owner of me".to_string()));
+    let str_ref_clone1 = str0.clone();
+    let str_ref_clone2 = Rc::clone(&str0);
+    str_ref_clone2.borrow_mut().push_str("refcell_rc_mix");
+    str_ref_clone1.borrow_mut().push_str(" 2 times");
+    /*
+    RefCell { value: "multi owner of merefcell_rc_mix 2 times" }
+    RefCell { value: "multi owner of merefcell_rc_mix 2 times" }
+    RefCell { value: "multi owner of merefcell_rc_mix 2 times" }
+    */
+    println!("{:?}\n{:?}\n{:?}", str0, str_ref_clone1, str_ref_clone2);
+}
+
+/// Cell::from_mut和as_slice_of_cells方法
+fn is_even(i: i32) -> bool {
+    i % 2 == 0
+}
+fn retain_even(nums: &mut Vec<i32>) {
+    /*let mut i = 0;
+    for num in nums.iter().filter(|&n| is_even(*n)) {
+        nums[i] = *num;
+        i += 1;
+    }
+    nums.truncate(i);*/
+    let slice: &[Cell<i32>] = Cell::from_mut(&mut nums[..]).as_slice_of_cells();
+    let mut i = 0;
+    for num in slice.iter().filter(|n| is_even(n.get())) {
+        slice[i].set(num.get());
+        i += 1;
+    }
+    nums.truncate(i);
+    println!("nums {:?}", nums);
+}
 pub fn practice() {
     example_box();
     example_leak_of_box();
     example_mybox();
     deref_example();
+    rc_example();
+    example_rc_all();
+    arc_example();
+    example_cell();
+    refcell_example();
+    refcell_rc_mix();
+    let mut vec_val: Vec<i32> = vec![12, 34, 55, 66, 77, 99];
+    retain_even(&mut vec_val);
 }
