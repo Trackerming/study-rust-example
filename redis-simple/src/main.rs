ed5b48bcbdbc4ddc7fce58c807dc955b4901838f
Trackerming <1591724804@qq.com>
2023-10-25 18:00:16 +0800

    [redis简单示例]使用HashMap实现简易支持SET、GET指令的redis server

--- redis-simple/src/main.rs
+++ redis-simple/src/main.rs
@@ -1,4 +1,39 @@
+use mini_redis::{Connection, Frame};
+use std::collections::HashMap;
+use tokio::net::{TcpListener, TcpStream};
+
 #[tokio::main]
 async fn main() {
-    println!("tokio main");
+    let listener = TcpListener::bind("127.0.0.1:8888").await.unwrap();
+    loop {
+        let (socket, sock_addr) = listener.accept().await.unwrap();
+        println!("accept sock_addr: {:?}", sock_addr);
+        tokio::spawn(async move {
+            process(socket).await;
+        });
+    }
+}
+
+async fn process(socket: TcpStream) {
+    use mini_redis::Command::{self, Get, Set};
+    let mut db = HashMap::new();
+    let mut connection = Connection::new(socket);
+    while let Some(frame) = connection.read_frame().await.unwrap() {
+        println!("Got: {:?}", frame);
+        let resp = match Command::from_frame(frame).unwrap() {
+            Set(cmd) => {
+                db.insert(cmd.key().to_string(), cmd.value().to_vec());
+                Frame::Simple("OK".to_string())
+            }
+            Get(cmd) => {
+                if let Some(value) = db.get(cmd.key()) {
+                    Frame::Bulk(value.clone().into())
+                } else {
+                    Frame::Null
+                }
+            }
+            cmd => panic!("unimplement {:?}", cmd),
+        };
+        connection.write_frame(&resp).await.unwrap();
+    }
 }
