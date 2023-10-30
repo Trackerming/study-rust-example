d6da494d5b11ede3fa12ea47718b6451b81f4078
Trackerming <1591724804@qq.com>
2023-10-30 14:10:22 +0800

    [实现链表示例]实现链表的peek和Iterator功能涵into_iter、iter和iter_mut方法

--- list/src/optimal_list.rs
+++ list/src/optimal_list.rs
@@ -30,6 +30,67 @@ impl<T> List<T> {
             node.elem
         })
     }
+
+    pub fn peek(&self) -> Option<&T> {
+        self.head.as_ref().map(|node| &node.elem)
+    }
+
+    pub fn peek_mut(&mut self) -> Option<&mut T> {
+        self.head.as_mut().map(|node| &mut node.elem)
+    }
+
+    pub fn into_iter(self) -> IntoIter<T> {
+        IntoIter(self)
+    }
+
+    pub fn iter<'a>(&'a self) -> Iter<'a, T> {
+        Iter {
+            next: self.head.as_deref(),
+        }
+    }
+
+    pub fn iter_mut<'a>(&'a mut self) -> IterMut<'a, T> {
+        IterMut {
+            next: self.head.as_deref_mut(),
+        }
+    }
+}
+
+pub struct IntoIter<T>(List<T>);
+
+impl<T> Iterator for IntoIter<T> {
+    type Item = T;
+    fn next(&mut self) -> Option<Self::Item> {
+        self.0.pop()
+    }
+}
+
+pub struct Iter<'a, T> {
+    next: Option<&'a Node<T>>,
+}
+
+impl<'a, T> Iterator for Iter<'a, T> {
+    type Item = &'a T;
+    fn next(&mut self) -> Option<Self::Item> {
+        self.next.map(|node| {
+            self.next = node.next.as_deref();
+            &node.elem
+        })
+    }
+}
+
+pub struct IterMut<'a, T> {
+    next: Option<&'a mut Node<T>>,
+}
+
+impl<'a, T> Iterator for IterMut<'a, T> {
+    type Item = &'a mut T;
+    fn next(&mut self) -> Option<Self::Item> {
+        self.next.take().map(|node| {
+            self.next = node.next.as_deref_mut();
+            &mut node.elem
+        })
+    }
 }
 
 impl<T> Drop for List<T> {
@@ -71,5 +132,67 @@ mod test {
             list.push(i);
         }
         drop(list);
+    }
+
+    #[test]
+    fn test_peek() {
+        let mut list = List::new();
+        assert_eq!(list.peek(), None);
+        assert_eq!(list.peek_mut(), None);
+
+        for i in 0..3 {
+            list.push(i);
+        }
+        assert_eq!(list.peek(), Some(&2));
+        assert_eq!(list.peek_mut(), Some(&mut 2));
+        list.peek_mut().map(|value| {
+            *value = 3333;
+        });
+        assert_eq!(list.peek(), Some(&3333));
+        assert_eq!(list.peek_mut(), Some(&mut 3333));
+    }
+
+    #[test]
+    fn test_into_iter() {
+        let mut list = List::new();
+        for i in 0..3 {
+            list.push(i);
+        }
+        let mut iter = list.into_iter();
+        assert_eq!(iter.next(), Some(2));
+        assert_eq!(iter.next(), Some(1));
+        assert_eq!(iter.next(), Some(0));
+        assert_eq!(iter.next(), None);
+    }
+
+    #[test]
+    fn test_iter() {
+        let mut list = List::new();
+        for i in 0..3 {
+            list.push(i);
+        }
+        let mut iter = list.iter();
+        assert_eq!(iter.next(), Some(&2));
+        assert_eq!(iter.next(), Some(&1));
+        assert_eq!(iter.next(), Some(&0));
+        assert_eq!(iter.next(), None);
+    }
+
+    #[test]
+    fn test_iter_mut() {
+        let mut list = List::new();
+        for i in 0..3 {
+            list.push(i);
+        }
+        {
+            let mut iter = list.iter_mut();
+            assert_eq!(iter.next(), Some(&mut 2));
+            assert_eq!(iter.next(), Some(&mut 1));
+            assert_eq!(iter.next(), Some(&mut 0));
+        }
+        let mut iter2 = list.iter_mut();
+        iter2.next().map(|value| *value = 12);
+        let mut iter3 = list.iter();
+        assert_eq!(iter3.next(), Some(&12));
     }
 }
