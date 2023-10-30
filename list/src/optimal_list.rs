ec2ac8b2734211339a03fe6e75be568517651752
Trackerming <1591724804@qq.com>
2023-10-30 10:45:26 +0800

    [实现链表示例]优化Link类型并采用Option的take替代mem::replace

--- list/src/optimal_list.rs
+++ list/src/optimal_list.rs
@@ -1,1 +1,75 @@
+use std::mem;
 
+type Link<T> = Option<Box<Node<T>>>;
+
+struct Node<T> {
+    elem: T,
+    next: Link<T>,
+}
+
+pub struct List<T> {
+    head: Link<T>,
+}
+
+impl<T> List<T> {
+    pub fn new() -> Self {
+        List { head: None }
+    }
+
+    pub fn push(&mut self, elem: T) {
+        let new_node = Box::new(Node {
+            elem,
+            next: self.head.take(),
+        });
+        self.head = Some(new_node);
+    }
+
+    pub fn pop(&mut self) -> Option<T> {
+        self.head.take().map(|node| {
+            self.head = node.next;
+            node.elem
+        })
+    }
+}
+
+impl<T> Drop for List<T> {
+    fn drop(&mut self) {
+        let mut current_link = self.head.take();
+        while let Some(mut box_node) = current_link {
+            current_link = box_node.next.take();
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::List;
+
+    #[test]
+    fn basic() {
+        let mut list: List<i32> = List::new();
+        assert_eq!(list.pop(), None);
+        // push元素
+        list.push(1);
+        list.push(2);
+        list.push(3);
+        assert_eq!(list.pop(), Some(3));
+        assert_eq!(list.pop(), Some(2));
+        list.push(4);
+        list.push(5);
+        assert_eq!(list.pop(), Some(5));
+        assert_eq!(list.pop(), Some(4));
+
+        assert_eq!(list.pop(), Some(1));
+        assert_eq!(list.pop(), None);
+    }
+
+    #[test]
+    fn long_list() {
+        let mut list = List::new();
+        for i in 0..100000 {
+            list.push(i);
+        }
+        drop(list);
+    }
+}
