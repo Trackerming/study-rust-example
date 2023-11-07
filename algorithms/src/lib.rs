0c7e1d7df6a9c08d4f70a28504233c9b9c3c1cb3
Trackerming <1591724804@qq.com>
2023-11-07 15:43:26 +0800

    [算法篇]添加长度为N元素小于N的数组中查找重复元素(重点:rust数组遍历的时候如何绕过同时拥有多个引用其中还包含mut引用)

--- algorithms/src/lib.rs
+++ algorithms/src/lib.rs
@@ -1,3 +1,4 @@
 pub mod binary_search_tree_from_preorder_traversal;
 // 二维数组查询
 pub mod array_2d_search;
+pub mod repeat_of_array;
---
+++ algorithms/src/repeat_of_array.rs
@@ -0,0 +1,48 @@
+use std::ops::Deref;
+
+/// 从数组中找出任意重复的元素，不添加额外的存储和排序等方式
+/// 数组的特点，长度为N，则元素为小于N的数据
+/// 从前到后遍历数组，将元素与对应index的数据调换位置，如果调换的时候对应index上的元素是相等的，说明重复的找到
+pub fn find_repeat_of_array(array: &mut Vec<i32>) -> Option<i32> {
+    if array.len() <= 1 {
+        return None;
+    }
+    let iter = array.iter();
+    for ele in iter.enumerate() {
+        // 交换元素的位置
+        if ele.0 != *ele.1 as usize {
+            // 如果元素相等则说明是重复的元素
+            if *ele.1 == *array.get(*ele.1 as usize).unwrap() {
+                return Some(*ele.1);
+            } else {
+                unsafe {
+                    let ptr = array.get(0)? as *const i32 as *mut i32;
+                    std::ptr::swap(ptr.add(ele.0), ptr.add(*ele.1 as usize));
+                }
+            }
+        }
+    }
+    return None;
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_find_repeat_of_array() {
+        let mut array = vec![2, 3, 1, 0, 2, 5, 3];
+        // [1, 3, 2, 0, 2, 5, 3]
+        // [1, 0, 2, 3, 2, 5, 3]
+        // [1, 0, 2, 3, 2, 5, 3]
+        // [1, 0, 2, 3, 2...
+        let result = find_repeat_of_array(&mut array);
+        assert_eq!(result, Some(2));
+        let mut array = vec![1, 0, 3, 2, 5, 4];
+        let result = find_repeat_of_array(&mut array);
+        assert_eq!(result, None);
+        let mut array = vec![0];
+        let result = find_repeat_of_array(&mut array);
+        assert_eq!(result, None);
+    }
+}
