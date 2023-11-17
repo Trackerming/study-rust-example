b302767b0a30369616fa298a2d3f3ec16f24667b
Trackerming <1591724804@qq.com>
2023-11-17 14:48:17 +0800

    [crypto篇]DES实现明文轮函数处理过程包括E扩展与密钥进行异或S盒子变换P变换以及轮的实现

--- crypto/des/src/des_char_impl.rs
+++ crypto/des/src/des_char_impl.rs
@@ -1,7 +1,8 @@
 use crate::permutation_list::*;
 use std::cell::RefCell;
-use std::ops::Deref;
+use std::ops::{Deref, DerefMut};
 use std::rc::Rc;
+use std::sync::Arc;
 
 struct DESChar {
     // 用64字节长度的二进制字符串表示key，比如"0100101010001111101...00101"
@@ -10,6 +11,19 @@ struct DESChar {
     init_permutation_key: Vec<u8>,
     // 16轮有16个子密钥
     sub_keys: Vec<Vec<u8>>,
+}
+
+// 根据给定表格进行置换操作
+fn permutation_by_table(origin: &[u8], table: &[u8], src_len: usize, dest_len: usize) -> Vec<u8> {
+    let mut result = vec![0; dest_len];
+    assert_eq!(origin.len(), src_len);
+    for (index, pos) in table.iter().enumerate() {
+        let pos_value = (*pos - 1) as usize;
+        assert!(pos_value < src_len);
+        result[index] = origin[pos_value];
+    }
+    assert_eq!(result.len(), dest_len);
+    return result;
 }
 
 impl DESChar {
@@ -28,13 +42,7 @@ impl DESChar {
             .map(|c| c.to_digit(2).unwrap() as u8)
             .collect();
         // 进行置换
-        let mut result: [u8; 56] = [0; 56];
-        for (index, pos) in INIT_KEY_PERMUTATION.iter().enumerate() {
-            let pos_value = (*pos - 1) as usize;
-            assert!(pos_value < key_chars.len());
-            result[index] = key_chars[pos_value];
-        }
-        self.init_permutation_key = result.to_vec();
+        self.init_permutation_key = permutation_by_table(&key_chars, &INIT_KEY_PERMUTATION, 64, 56);
     }
 
     fn get_l_and_r_of_init_key(&mut self) -> (&mut [u8], &mut [u8]) {
@@ -60,20 +68,118 @@ impl DESChar {
                 right_clone.clone().borrow().to_vec(),
             ]
             .concat();
-            let mut result: [u8; 48] = [0; 48];
-            for (index, pos) in SUB_KEY_PERMUTATION.iter().enumerate() {
-                let pos_value = (*pos - 1) as usize;
-                assert!(pos_value < combined.len());
-                result[index] = combined[pos_value];
-            }
-            self.sub_keys.push(result.to_vec());
+            self.sub_keys.push(permutation_by_table(
+                &combined,
+                &SUB_KEY_PERMUTATION,
+                56,
+                48,
+            ));
         }
+    }
+
+    fn plaintext_init_permutation(&self, plaintext: &str) -> Vec<u8> {
+        // 先不考虑分组，按照标准明文的64个bit进行计算
+        let plaintext_chars: Vec<u8> = plaintext
+            .chars()
+            .map(|c| c.to_digit(2).unwrap() as u8)
+            .collect();
+        return permutation_by_table(&plaintext_chars, &INIT_PLAINTEXT_PERMUTATION, 64, 64);
+    }
+
+    // E变换
+    fn extend_right(&self, right: &mut [u8]) -> Vec<u8> {
+        // 每四位一组对1和4分别向左右扩展
+        return permutation_by_table(&right, &EXTEND_R_PLAINTEXT_PERMUTATION, 32, 48);
+    }
+
+    // S盒子变换
+    fn s_box_convert(&self, input: &[u8], s_box: &[u8; 64]) -> [u8; 32] {
+        let mut result: [u8; 32] = [0; 32];
+        for i in 0..8 {
+            let current_input = &input[(i * 6)..((i + 1) * 6)];
+            // 取出input的坐标，bit1和2代表行 b3～b6代表列
+            let rows = current_input[0] << 1 | current_input[5];
+            let cols = (current_input[1] << 3)
+                | (current_input[2] << 2)
+                | (current_input[3] << 1)
+                | current_input[4];
+            let index = (rows * 16 + cols) as usize;
+            assert!(index < 64);
+            let value = s_box[index];
+            let rslt: Vec<u8> = format!("{:b}", value)
+                .chars()
+                .map(|char| char.to_digit(2).unwrap() as u8)
+                .collect();
+            result[(i * 4)..((i + 1) * 4)].clone_from_slice(&rslt);
+        }
+        return result;
+    }
+
+    // P盒子变换
+    fn p_box_permutation(&self, input: &[u8]) -> Vec<u8> {
+        return permutation_by_table(&input, &P_R_SUB_PLAINTEXT_PERMUTATION, 32, 32);
+    }
+
+    // 算法轮对R的处理，包括E扩展、key_i异或、S盒子变换、P盒置换、L(上一轮的R)异或
+    fn round_handle_r_func(
+        &self,
+        left: &Vec<u8>,
+        prev_right: &mut Vec<u8>,
+        current_round: usize,
+    ) -> Vec<u8> {
+        let mut current_right: Vec<u8> = vec![];
+        // 对32位的right进行E扩展变成48位
+        let right = self.extend_right(prev_right);
+        // 与当前轮的密钥进行异或
+        let mut xor_with_key: Vec<u8> = right
+            .iter()
+            .zip(self.sub_keys[current_round].iter())
+            .map(|(a, b)| (*a) ^ (*b))
+            .collect();
+        // S盒变换
+        let s_result = self.s_box_convert(&mut xor_with_key, &S[current_round]);
+        // P盒变换
+        let mut p_result = self.p_box_permutation(&s_result);
+        current_right = p_result
+            .iter()
+            .zip(left.iter())
+            .map(|(a, b)| a ^ b)
+            .collect();
+        assert_eq!(current_right.len(), 32);
+        return current_right;
+    }
+
+    fn round_handle(&self, init_perm_plaintext: &mut [u8; 64]) {
+        let (left, right) = init_perm_plaintext.split_at_mut(32);
+        let mut left_clone = Arc::new(RefCell::new(left.to_vec()));
+        let mut right_clone = Arc::new(RefCell::new(right.to_vec()));
+        // 经过16轮计算得到最终的L和R
+        for i in 0..ROUND {
+            let mut current_r = self.round_handle_r_func(
+                left_clone.clone().borrow().deref(),
+                right_clone.clone().borrow_mut().deref_mut(),
+                i,
+            );
+            // 上一轮的R变为这一轮的L
+            left_clone
+                .clone()
+                .borrow_mut()
+                .clone_from_slice(left_clone.clone().borrow().as_slice());
+            right_clone
+                .clone()
+                .borrow_mut()
+                .clone_from_slice(current_r.as_slice());
+        }
+        // 合并L和R并进行最终的逆变换
     }
 }
 
 #[cfg(test)]
 mod test {
     use crate::des_char_impl::DESChar;
+    use crate::permutation_list::{EXTEND_R_PLAINTEXT_PERMUTATION, S};
+    use std::cell::RefCell;
+    use std::sync::Arc;
 
     #[test]
     fn test_init_key_per() {
@@ -87,6 +193,54 @@ mod test {
             des_char.init_permutation_key.len()
         );
         des_char.sub_key_permutation();
-        println!("{:?}", des_char.sub_keys);
+        for sub_key in des_char.sub_keys.iter() {
+            println!("{:?}", sub_key);
+        }
+        let plaintext = "0000000100100011010001010110011110001001101010111100110111101111";
+        let init_permutation_plaintext = des_char.plaintext_init_permutation(plaintext);
+        println!("{:?}", init_permutation_plaintext);
+        let mut array = [
+            32, 1, 2, 3, 4, 5, 4, 5, 6, 7, 8, 9, 8, 9, 10, 11, 12, 13, 12, 13, 14, 15, 16, 17, 16,
+            17, 18, 19, 20, 21, 20, 21, 22, 23, 24, 25, 24, 25, 26, 27, 28, 29, 28, 29, 30, 31, 32,
+            1,
+        ];
+        array.sort();
+
+        println!("{:?}", array);
+    }
+
+    #[test]
+    fn test_s_box() {
+        let mut des_char = DESChar::new(
+            "0001001100110100010101110111100110011011101111001101111111110001".to_string(),
+        );
+        let input = [
+            0, 1, 1, 0, 1, 0, 0, 1, 1, 0, 1, 0, 0, 1, 1, 0, 1, 0, 0, 1, 1, 0, 1, 0, 0, 1, 1, 0, 1,
+            0, 0, 1, 1, 0, 1, 0, 0, 1, 1, 0, 1, 0, 0, 1, 1, 0, 1, 0,
+        ];
+        let result = des_char.s_box_convert(&input, &S[0]);
+        println!("{:?}", result);
+        println!("{}", format!("{:b}", 12));
+    }
+
+    #[test]
+    fn test_slice_copy() {
+        let left = [12, 34, 56];
+        let right = [78, 90, 23];
+        let right2 = [45, 67, 89];
+        let mut left_clone = Arc::new(RefCell::new(left.to_vec()));
+        let mut right_clone = Arc::new(RefCell::new(right.to_vec()));
+        println!("before replace left: {:?}", left_clone.clone().borrow());
+        println!("before replace right: {:?}", right_clone.clone().borrow());
+        left_clone
+            .clone()
+            .borrow_mut()
+            .clone_from_slice(right_clone.clone().borrow().as_slice());
+        right_clone
+            .clone()
+            .borrow_mut()
+            .clone_from_slice(right2.as_slice());
+        println!("after replace left: {:?}", left_clone.clone().borrow());
+        println!("after replace right: {:?}", right_clone.clone().borrow());
     }
 }
