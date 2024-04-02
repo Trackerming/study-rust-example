e9c3ff1e8021849457ea77e7fa957d9ae001fbff
Trackerming <1591724804@qq.com>
2024-04-02 14:56:31 +0800

    [crypto]MerkleTree添加BTC形式的double hash计算

--- crypto/data_structure/src/merkle_trie.rs
+++ crypto/data_structure/src/merkle_trie.rs
@@ -16,17 +16,25 @@ impl MerkleTrieNode {
         }
     }
 
-    pub fn compute_hash(left_hash: &[u8], right_hash: &[u8]) -> Vec<u8> {
+    pub fn compute_hash(left_hash: &[u8], right_hash: &[u8], is_btc: bool) -> Vec<u8> {
         let mut sha256 = Box::new(Sha256::new());
         sha256.input(left_hash);
         sha256.input(right_hash);
         let mut out = vec![0u8; sha256.output_bytes()];
         sha256.result(&mut out);
-        out
+        if is_btc {
+            let mut sha256 = Box::new(Sha256::new());
+            sha256.input(&out);
+            let mut result = vec![0u8; sha256.output_bytes()];
+            sha256.result(&mut result);
+            result
+        } else {
+            out
+        }
     }
 
     // 递归构建树结构
-    pub fn build_tree(data: Vec<Vec<u8>>) -> Option<Box<MerkleTrieNode>> {
+    pub fn build_tree(data: Vec<Vec<u8>>, is_btc: bool) -> Option<Box<MerkleTrieNode>> {
         if data.is_empty() {
             return None;
         }
@@ -36,11 +44,21 @@ impl MerkleTrieNode {
         let mid = data.len() / 2;
         let left_data = data[..mid].to_vec();
         let right_data = data[mid..].to_vec();
+        println!(
+            "mid: {:?}, left: {:?} right: {:?}",
+            mid, left_data, right_data
+        );
 
-        let left_node = MerkleTrieNode::build_tree(left_data);
-        let right_node = MerkleTrieNode::build_tree(right_data);
+        let left_node = MerkleTrieNode::build_tree(left_data, is_btc);
+        let right_node = MerkleTrieNode::build_tree(right_data, is_btc);
+        println!(
+            "mid: {:?}, left: {:?} right: {:?}",
+            mid, left_node, right_node
+        );
         let hash = match (&left_node, &right_node) {
-            (Some(left), Some(right)) => MerkleTrieNode::compute_hash(&left.hash, &right.hash),
+            (Some(left), Some(right)) => {
+                MerkleTrieNode::compute_hash(&left.hash, &right.hash, is_btc)
+            }
             (Some(node), None) | (None, Some(node)) => node.hash.clone(),
             _ => Vec::new(),
         };
@@ -86,9 +104,30 @@ mod merkle_tree_test {
                 212, 125, 34, 29, 147, 84, 180, 118, 6, 161, 131, 148, 187, 199, 235,
             ],
         });
-        if let Some(node) = MerkleTrieNode::build_tree(data) {
+        if let Some(node) = MerkleTrieNode::build_tree(data, false) {
             println!("tree: {:?}", node);
             assert_eq!(node, expected_tree);
         }
     }
+
+    /*    #[test]
+    fn test_btc_block_120008() {
+        let blocks = vec![
+            // 1
+            enc_dec::hex_string_to_bytes("c338a4bc83d859feec4f9fab9bea5d5cdfa6d80804d026649f9e08e28a1126f5"),
+            // 2
+            enc_dec::hex_string_to_bytes("31009eb60976f1daa55da93c2d48ab3bb3f183721b376bb4bf6966e6768ddadf"),
+            // 3
+            enc_dec::hex_string_to_bytes("50ce49e1f542dc74046b14757c5b03b127802d7e01d8662612e417d6d7edc5ae"),
+            // 4
+            enc_dec::hex_string_to_bytes("2973971ee8dfd69a44954ac35150a65900a871f9402ab60a63708d2774d125cc"),
+            // 5
+            enc_dec::hex_string_to_bytes("08edefb2eec45dfc9e6228b34988c36d25e2dc5f384a4c3dea2bf5fdb26aefd4"),
+            // 6
+            enc_dec::hex_string_to_bytes("da5423baae6eeefa3535a8fb05304214b556b9ceed699316a9dd84b00f685326"),
+        ];
+        if let Some(node) = MerkleTrieNode::build_tree(blocks, true) {
+            println!("tree: {:?}", node);
+        }
+    }*/
 }
