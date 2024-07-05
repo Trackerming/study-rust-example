d0244eb19b16c06708b6550f1686b47fc24a2847
Trackerming <trackerming@qq.com>
2024-07-05 21:05:56 +0800

    [crypto]以范型方式定义模运算的相关计算

--- crypto/crypto_util/src/lib.rs
+++ crypto/crypto_util/src/lib.rs
@@ -1,3 +1,4 @@
 pub mod base_compute;
+mod mod_operate;
 pub mod point;
 mod prime;
---
+++ crypto/crypto_util/src/mod_operate.rs
@@ -0,0 +1,139 @@
+use std::fmt::Debug;
+// + - * / %
+use std::ops::{Add, Div, Mul, Rem, Sub};
+
+// 定义一个新的 trait Num，包含 Add, Sub, Mul, Div 这些 trait 的约束
+trait Num:
+    Add<Output = Self>
+    + Sub<Output = Self>
+    + Mul<Output = Self>
+    + Div<Output = Self>
+    + Rem<Output = Self>
+    + PartialEq
+    + PartialOrd
+    + Sized
+    + Debug
+    + Copy
+{
+}
+
+// 为所有满足条件的类型实现 Num trait
+impl<T> Num for T where
+    T: Add<Output = T>
+        + Sub<Output = T>
+        + Mul<Output = T>
+        + Div<Output = T>
+        + Rem<Output = T>
+        + PartialOrd
+        + PartialEq
+        + Debug
+        + Sized
+        + Copy
+{
+}
+
+pub trait ModOperate<T: Num> {
+    fn mod_abs(a: T, m: T) -> T;
+    // 求解模逆
+    fn mod_inverse(a: T, m: T) -> Option<T>;
+    // 求模幂
+    fn mod_exp(base: T, exponent: T, m: T) -> T;
+    // 欧几里得除法求商和余数
+    fn euclidean_division(a: T, b: T) -> (T, T);
+    // 扩展欧几里得算法，迭代法
+    fn extend_euclidean_algorithm(a: T, m: T) -> (T, T, T);
+    // 扩展欧几里得算法，递归法
+    fn extend_euclidean_recursive(a: T, m: T) -> (T, T);
+    // 求解两个数的最大公约数，采用欧几里得辗转相除法
+    fn gcd(a: T, b: T) -> T;
+}
+
+struct CryptoMod {}
+
+impl<T> ModOperate<T> for CryptoMod
+where
+    T: Num,
+    T: From<isize>,
+{
+    fn gcd(mut a: T, mut b: T) -> T {
+        if a < b {
+            std::mem::swap(&mut a, &mut b);
+        }
+        while b > 0.into() {
+            let rem = a % b;
+            a = b;
+            b = rem;
+        }
+        a
+    }
+
+    fn euclidean_division(a: T, b: T) -> (T, T) {
+        assert_ne!(b, 0.into());
+        (a / b, a % b)
+    }
+
+    fn extend_euclidean_algorithm(mut a: T, mut m: T) -> (T, T, T) {
+        let (mut x1, mut y1, mut x2, mut y2) = (1.into(), 0.into(), 0.into(), 1.into());
+        while m > 0.into() {
+            let (q, r) = Self::euclidean_division(a, m);
+            (a, m) = (m, r);
+            (x1, x2) = (x2, x1 - q * x2);
+            (y1, y2) = (y2, y1 - q * y2);
+        }
+        (a, x1, y1)
+    }
+
+    fn extend_euclidean_recursive(a: T, m: T) -> (T, T) {
+        return if m == 0.into() {
+            (1.into(), 0.into())
+        } else {
+            let (mut x, mut y) = Self::extend_euclidean_recursive(m, a % m);
+            (x, y) = (y, x - (a / m) * y);
+            (x, y)
+        };
+    }
+
+    fn mod_abs(a: T, m: T) -> T {
+        assert!(m > 0.into());
+        let mut result = a;
+        if a < m && a >= 0.into() {
+            result = a;
+        } else if a < 0.into() {
+            while result < 0.into() {
+                result = result + m;
+            }
+        } else {
+            result = a % m;
+        }
+        return result;
+    }
+
+    fn mod_inverse(a: T, m: T) -> Option<T> {
+        let (gcd_val, x, _) = Self::extend_euclidean_algorithm(a, m);
+        return if gcd_val == 1.into() {
+            return Some(Self::mod_abs(x, m));
+        } else {
+            None
+        };
+    }
+
+    fn mod_exp(base: T, exponent: T, m: T) -> T {
+        if m == 1.into() {
+            return 0.into();
+        }
+        let mut result = 1.into();
+        let mut base = base % m;
+        let mut exponent = exponent;
+        while exponent > 0.into() {
+            if exponent % (2.into()) == (1.into()) {
+                result = (result * base) % m;
+            }
+            exponent = exponent / (2.into());
+            base = (base * base) % m;
+        }
+        result
+    }
+}
+
+#[cfg(test)]
+mod mod_operate_test {}
